e623198ff0543aab7733b4416dc8b82c182fa746
Shruthi Benedicta <shruthibenedicta@gmail.com>
2019-08-21 21:17:33 +0530

    Test case update

---
+++ src/app/filter.pipe.spec.ts
@@ -0,0 +1,79 @@
+
+/* tslint:disable:no-unused-variable */
+ 
+import { FilterPipe } from './filter.pipe';
+
+describe('Pipe: Default', () => {
+   let pipe: FilterPipe;
+
+   beforeEach(() => {
+       pipe = new FilterPipe();
+   });
+
+   it('providing no value returns items', () => {
+       const items = null;
+       const searchText = 'New';
+       expect(pipe.transform(items, searchText).length).toBe(0);
+   });
+
+   it('providing a value returns value when the searchText is empty', () => {
+    const items = [{
+        'firstName': 'Persia',
+        'lastName': 'Paulraj',
+        'empId': '459012',
+        'userId': '5c600f2cd76fbc29a8501452'
+      },
+      {
+        'firstName': 'Indhu',
+        'lastName': 'Prakash',
+        'empId': '345678',
+        'userId': '5c600fe6b3de090868cc11c4'
+      }];
+    const searchText = '';
+    expect(pipe.transform(items, searchText).length).toBe(2);
+   });
+
+   it('Return the search result array, when the searchText is present and valid for ADD USER page', () => {
+    const items = [{
+        'firstName': 'Persia',
+        'lastName': 'Paulraj',
+        'empId': '459012',
+        'userId': '5c600f2cd76fbc29a8501452'
+      },
+      {
+        'firstName': 'Indhu',
+        'lastName': 'Prakash',
+        'empId': '345678',
+        'userId': '5c600fe6b3de090868cc11c4'
+      }];
+    const searchText = 'Paulraj';
+    expect(pipe.transform(items, searchText)[0]['lastName']).toBe('Paulraj');
+   });
+
+   it('Return the search result array, when the searchText is present and valid for ADD PROJECT page', () => {
+    const items = [{
+        '_id': '5c694bd2808f9616f44c6281',
+        'projectName': 'American Ship',
+        'startDate': '2019-02-22',
+        'endDate': '2020-02-14',
+        'priority': 22,
+        'manager': 'Indhu Prakash',
+        'status': 'Not Updated',
+        'noOfTasks': 0,
+        'projectId': '5c694bd2808f9616f44c6280'
+      },
+      {
+        '_id': '5d556d183bc60017acd74d4a',
+        'projectName': 'Sample Project',
+        'startDate': '2019-08-15',
+        'endDate': '2020-01-30',
+        'priority': 23,
+        'manager': 'Dennison Peter',
+        'status': 'In-Progress',
+        'noOfTasks': 0,
+        'projectId': '5d556d183bc60017acd74d49'
+      }];
+    const searchText = 'American Ship';
+    expect(pipe.transform(items, searchText)[0]['projectName']).toBe('American Ship');
+   });
+});
--- src/app/user-management.service.spec.ts
+++ src/app/user-management.service.spec.ts
@@ -1,12 +1,115 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
 
-// import { UserManagementService } from './user-management.service';
+import { UserManagementService } from './user-management.service';
 
-// fdescribe('UserManagementService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
+describe('UserManagementService', () => {
+    let httpClientSpy: {
+        get: jasmine.Spy, post: jasmine.Spy, put: jasmine.Spy,
+        delete: jasmine.Spy
+    };
+    let userManagementService: UserManagementService;
 
-//   it('should be created', () => {
-//     const service: UserManagementService = TestBed.get(UserManagementService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+    beforeEach(() => {
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put', 'delete']);
+        userManagementService = new UserManagementService(<any>httpClientSpy, );
+        TestBed.configureTestingModule({
+            imports: [HttpClientModule],
+            providers: [UserManagementService]
+        })
+    });
+
+    it('should be created', () => {
+        const service: UserManagementService = TestBed.get(UserManagementService);
+        expect(service).toBeTruthy();
+    });
+
+    it('#getUserService', () => {
+        const userList = [];
+        httpClientSpy.get.and.returnValue(of(userList));
+        userManagementService.getUserService().subscribe(templates =>
+            expect(templates).toEqual(userList, 'expected form templates'), fail);
+    });
+
+    it('#getProjectService', () => {
+        const projectList = [];
+        httpClientSpy.get.and.returnValue(of(projectList));
+        userManagementService.getProjectService().subscribe(templates =>
+            expect(templates).toEqual(projectList, 'expected form templates'), fail);
+    });
+
+    it('#getTaskService', () => {
+        const taskList = [];
+        httpClientSpy.get.and.returnValue(of(taskList));
+        userManagementService.getTaskService().subscribe(templates =>
+            expect(templates).toEqual(taskList, 'expected form templates'), fail);
+    });
+
+    it('#getTaskListViewService', () => {
+        const projectTaskList = [];
+        httpClientSpy.get.and.returnValue(of(projectTaskList));
+        userManagementService.getTaskListViewService().subscribe(templates =>
+            expect(templates).toEqual(projectTaskList, 'expected form templates'), fail);
+    });
+
+    it('#addNewUserService - post', () => {
+        const addNewUserService: any = {};
+        httpClientSpy.post.and.returnValue(of(addNewUserService));
+        userManagementService.addNewUserService(addNewUserService);
+    });
+
+    it('#addNewProjectService - post', () => {
+        const addNewProjectService: any = {};
+        httpClientSpy.post.and.returnValue(of(addNewProjectService));
+        userManagementService.addNewProjectService(addNewProjectService);
+    });
+
+    it('#addNewTaskService', () => {
+        const addNewTaskService: any = {};
+        httpClientSpy.post.and.returnValue(of(addNewTaskService));
+        userManagementService.addNewTaskService(addNewTaskService);
+    });
+
+    it('#addNewUserService - put', () => {
+        const addNewUserService: any = {};
+        const input = {
+            userId : '1'
+        }
+        httpClientSpy.put.and.returnValue(of(addNewUserService));
+        userManagementService.addNewUserService(input);
+    });
+
+    it('#addNewProjectService - put', () => {
+        const input = {
+            projectId : '1'
+        }
+        const addNewProjectService: any = {};
+        httpClientSpy.put.and.returnValue(of(addNewProjectService));
+        userManagementService.addNewProjectService(input);
+    });
+
+    it('#updateTaskService', () => {
+        const updateTaskService: any = {};
+        httpClientSpy.put.and.returnValue(of(updateTaskService));
+        userManagementService.updateTaskService(updateTaskService);
+    });
+
+    it('#deletUserService', () => {
+        const input = {
+            userId : '1'
+        }
+        const deletUserService: any = {};
+        httpClientSpy.delete.and.returnValue(of(deletUserService));
+        userManagementService.deletUserService(input);
+    });
+
+    it('#suspendProjectService', () => {
+        const input = {
+            projectId : '1'
+        }
+        const suspendProjectService: any = {};
+        httpClientSpy.delete.and.returnValue(of(suspendProjectService));
+        userManagementService.suspendProjectService(input);
+    });
+});
